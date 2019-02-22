949015e3452ea171f9056bfdd9d7b6d430ad150a
Kumar Baibhav <baibhav.k@husky.neu.edu>
2019-02-22 12:11:40 -0500

    Some more Junit

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/NetworkConnectionTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/NetworkConnectionTest.java
@@ -156,6 +156,34 @@ public class NetworkConnectionTest {
 		assertEquals(false, b);
 	}
 	
+	@Test
+	public void test_sendMalformedMessage() throws IOException, InterruptedException {
+		synchronized (this) {
+			
+			wait(1000);
+		}
+		InetSocketAddress hostAddress = new InetSocketAddress("localhost", 4444);
+		clientSocket = SocketChannel.open(hostAddress);
+		netConn = new NetworkConnection(clientSocket);
+		String str= Message.makeHelloMessage("hellow world").toString();
+		ByteBuffer wrapper = ByteBuffer.wrap(longString.getBytes());
+		serverSocketChannel.write(wrapper);
+		synchronized (this) {
+			
+			wait(1000);
+		}
+		Iterator<Message> messageItr = netConn.iterator();
+		//clientSocket.close();
+		//serverSocketChannel.write(src)
+		while (messageItr.hasNext()) {
+			//System.out.println(messageItr.next().getText());
+			messageItr.next().getText();
+		}
+		boolean b = netConn.sendMessage(Message.makeHelloMessage(""));
+		netConn.close();
+		assertEquals(false, b);
+	}
+	
 	@Test(expected = NoSuchElementException.class)
 	public void test_iterator_error() throws IOException, InterruptedException {
 		synchronized (this) {
@@ -232,4 +260,19 @@ public class NetworkConnectionTest {
 		netConn = new NetworkConnection(clientSocket2);
 		netConn.close();
 	}
+	
+	private String longString = "Based on your input, get a random alpha numeric string. The random string generator creates a series of numbers and letters that have no pattern. These can be helpful for creating security codes.\n" + 
+			"\n" + 
+			"\n" + 
+			"With this utility you generate a 16 character output based on your input of numbers and upper and lower case letters.  Random strings can be unique. Used in computing, a random string generator can also be called a random character string generator. This is an important tool if you want to generate a unique set of strings. The utility generates a sequence that lacks a pattern and is random.\n" + 
+			"\n" + 
+			"\n" + 
+			"Throughout time, randomness was generated through mechanical devices such as dice, coin flips, and playing cards. A mechanical method of achieving randomness can be more time and resource consuming especially when a large number of randomized strings are needed as they could be in statistical applications.  Computational random string generators replace the traditional mechanical devices. \n" + 
+			"\n" + 
+			"\n" + 
+			"Possible applications for a random string generator could be for statistical sampling, simulations, and cryptography.  For security reasons, a random string generator can be useful. \n" + 
+			"\n" + 
+			"\n" + 
+			"The generation of this type of random string can be a common or typical task in computer programming.  Some forms of randomness concern hash or seach algorithms.  Another task that is random concerns selecting music tracks.";
+			
 }
