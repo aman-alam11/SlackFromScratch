cd9d1b67c377f9299eb5d7179964578e83c5cb37
mitresh <pandya.mi@husky.neu.edu>
2019-03-06 12:42:50 -0500

    added chat dao and pojo

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/JPAService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/JPAService.java
@@ -1,5 +1,6 @@
-package edu.northeastern.ccs.im.database;
+package com.msd.jpa;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -12,10 +13,16 @@ public class JPAService {
 	private UserDao ud;
 	
 	/**
+	 * Creating an object of ChatDao to call the methods to perform operations on chat.
+	 */
+	private ChatDao cd;
+	
+	/**
 	 * Constructor to initialize userdao object.
 	 */
-	JPAService(){
+	public JPAService(){
 		ud = new UserDao();
+		cd = new ChatDao();
 	}
 	
 	/**
@@ -71,5 +78,52 @@ public class JPAService {
 	 */
 	public void closeSessionFactory() {
 		ud.closeSessionFactory();
+	}
+	
+	/**
+	 * Create a new message in chat. 
+	 * @param from_id
+	 * @param to_id
+	 * @param msg
+	 * @param reply_to
+	 * @param created
+	 * @param expiery
+	 * @param grpMsg
+	 * @param isDelivered
+	 */
+	public void createChatMessage(int from_id, int to_id, String msg, int reply_to, Date created, Date expiery, Boolean grpMsg, Boolean isDelivered) {
+		cd.create(from_id, to_id, msg, reply_to, created, expiery, grpMsg, isDelivered);
+	}
+	
+	/**
+	 * List all messages for a user or a group.
+	 * @param receiver_id
+	 * @return
+	 */
+	public List<Chat> findByReceiver(int receiver_id) {
+		return cd.findByReceiver(receiver_id);
+	}
+	
+	/**
+	 * Delete the chat for a user or a group.
+	 * @param receiver_id
+	 */
+	public void deleteChatByReceiver(int receiver_id) {
+		cd.deleteChatByReceiver(receiver_id);
+	}
+	
+	/**
+	 * Delete a particular message.
+	 * @param id
+	 */
+	public void deleteMessage(int id) {
+		cd.delete(id);
+	}
+	
+	/**
+	 * Close a session factory once the operations are done.
+	 */
+	public void closeChatSessionFactory() {
+		cd.closeSessionFactory();
 	}
 }
