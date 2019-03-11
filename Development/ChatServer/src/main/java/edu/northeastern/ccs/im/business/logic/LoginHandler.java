656600e05db00fb37c2025cc5356c121f1b79e24
aman11 <alam.sy@husky.neu.edu>
2019-03-10 21:50:08 -0400

    connected server side handlers with auth layer

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/business/logic/LoginHandler.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/business/logic/LoginHandler.java
@@ -1,29 +1,35 @@
 package edu.northeastern.ccs.im.business.logic;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
+import java.util.logging.Logger;
+
+import edu.northeastern.ccs.im.auth.SessionFactory;
+import edu.northeastern.ccs.im.database.JPAService;
 import edu.northeastern.ccs.im.model.LoginCredentials;
-import edu.northeastern.ccs.im.model.User;
 
 public class LoginHandler implements MessageHandler {
-	
-	private Gson gson;
 
-	public LoginHandler() {
-		gson = new Gson();
-	}
-	@Override
-	public boolean handleMessage(String user, String message) {
-		try {
-			LoginCredentials lgn = gson.fromJson(message, LoginCredentials.class);
-			System.out.println(lgn.getUserName());
-			System.out.println(lgn.getPassword());
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+  private Gson gson;
+
+  public LoginHandler() {
+    gson = new Gson();
+  }
+
+  @Override
+  public boolean handleMessage(String user, String message) {
+    try {
+      LoginCredentials lgn = gson.fromJson(message, LoginCredentials.class);
+
+      // TODO: Use the overloaded method in interface and use the injected jpaservice here
+      SessionFactory sessionFactory = SessionFactory.getInstance(lgn.getUserName(), lgn.getPassword(),
+              new JPAService());
+
+      return sessionFactory.login();
+    } catch (Exception e) {
+      Logger.getLogger(this.getClass().getSimpleName()).info(e.getMessage());
+      return false;
+    }
+  }
 
 }
--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/business/logic/MessageHandler.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/business/logic/MessageHandler.java
@@ -1,9 +1,11 @@
 package edu.northeastern.ccs.im.business.logic;
 
-import edu.northeastern.ccs.im.model.User;
+import edu.northeastern.ccs.im.database.JPAService;
 
 public interface MessageHandler {
 	
 	boolean handleMessage(String user, String message);
 
+	// TODO: ??
+	// boolean handleMessage(String user, String message, JPAService jpaService);
 }
