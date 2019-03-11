c09b556a3f2de1b865fab20fa6b2860c860fb690
Kumar Baibhav <baibhav.k@husky.neu.edu>
2019-03-11 18:45:52 -0400

    Using correct connection

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/clientmenu/MainClass.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/clientmenu/MainClass.java
@@ -1,14 +1,14 @@
 package edu.northeastern.ccs.im.clientmenu;
 
 import edu.northeastern.ccs.im.client.communication.Connection;
-import edu.northeastern.ccs.im.clientmenu.firstlevel.FirstLevelModel;
+import edu.northeastern.ccs.im.client.communication.SocketConnection;
 
 public class
 MainClass {
 
   public static void main(String[] args) {
 
-    Connection topModelLayer =  new FirstLevelModel(ClientConstants.URL, ClientConstants.PORT);
+    Connection topModelLayer =  SocketConnection.getInstance(ClientConstants.URL, ClientConstants.PORT);
     ClientHandler clientHandler = new ClientHandler(topModelLayer);
     clientHandler.initClientOperations();
   }
