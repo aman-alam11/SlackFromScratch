426495d538fdbd0151c314462b8b7651416dab2c
mitresh <pandya.mi@husky.neu.edu>
2019-03-12 13:31:18 -0400

    added jpa tests that covers 100% user

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/database/JPAServiceTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/database/JPAServiceTest.java
@@ -5,6 +5,9 @@ import org.hibernate.cfg.Configuration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.persistence.NoResultException;
+
 import static org.junit.Assert.assertEquals;
 
 public class JPAServiceTest {
@@ -38,9 +41,15 @@ public class JPAServiceTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testCreateBlankUser(){
+    public void testCreateUserWithNullName(){
         JPAService jpaS = new JPAService(sessionFactory);
-        jpaS.createUser(null,"a@a.com",null);
+        jpaS.createUser(null,"a@a.com","asdf");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateUserWithNullPassword(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.createUser("alice","a@a.com",null);
     }
 
     @Test
@@ -50,6 +59,45 @@ public class JPAServiceTest {
         User alice = jpaS.findUserByName("Alice");
         assertEquals("a@a.com",alice.getEmail());
     }
+
+    @Test
+    public void testUpdateUser(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.createUser("Alice","a@a.com","alice");
+        User alice = jpaS.findUserByName("Alice");
+        assertEquals("a@a.com",alice.getEmail());
+        jpaS.updateUser(alice.getId(),alice.getName(),"alice@new.com",alice.getPassword());
+        User newAlice = jpaS.findUserByName("Alice");
+        assertEquals("alice@new.com",newAlice.getEmail());
+    }
+
+    @Test(expected = NoResultException.class)
+    public void testHashForUser(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.getHashFromUsername(null);
+    }
+
+    @Test
+    public void testNullUserEmail(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.createUser("Alice",null,"alice");
+        User alice = jpaS.findUserByName("Alice");
+        assertEquals("", alice.getEmail());
+    }
+
+    @Test
+    public void testNullEmailGet(){
+        User tempUesr = new User();
+        assertEquals("",tempUesr.getEmail());
+    }
+
+    @Test
+    public void testNullEmailSet(){
+        User tempUesr = new User();
+        tempUesr.setEmail(null);
+        assertEquals("",tempUesr.getEmail());
+    }
+
     @After
     public void after() {
         sessionFactory.close();
