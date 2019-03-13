75e9b9eb473e73359f68c4e7a061965b04c63402
aman11 <alam.sy@husky.neu.edu>
2019-03-13 19:02:08 -0400

    added missing hook for async operation

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/clientmenu/firstlevel/UserChatModelLayer.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/clientmenu/firstlevel/UserChatModelLayer.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 
 import java.util.Scanner;
 
+import edu.northeastern.ccs.im.client.communication.AsyncListener;
 import edu.northeastern.ccs.im.client.communication.Connection;
+import edu.northeastern.ccs.im.client.communication.SocketConnection;
 import edu.northeastern.ccs.im.clientmenu.clientinterfaces.CoreOperation;
 import edu.northeastern.ccs.im.clientmenu.clientutils.GenerateLoginCredentials;
 import edu.northeastern.ccs.im.clientmenu.models.UserChat;
@@ -12,7 +14,7 @@ import edu.northeastern.ccs.im.message.MessageJson;
 import edu.northeastern.ccs.im.message.MessageType;
 import edu.northeastern.ccs.im.view.FrontEnd;
 
-public class UserChatModelLayer implements CoreOperation {
+public class UserChatModelLayer implements CoreOperation, AsyncListener {
 
   private String userToChatWith;
 
@@ -29,12 +31,22 @@ public class UserChatModelLayer implements CoreOperation {
     userChat.setMsg(message);
 
     // Send Object
-    MessageJson messageJson = new MessageJson(GenerateLoginCredentials.getUsername(), MessageType.USER_CHAT, new Gson().toJson(userChat));
+    MessageJson messageJson = new MessageJson(GenerateLoginCredentials.getUsername(),
+            MessageType.USER_CHAT, new Gson().toJson(userChat));
+    ((SocketConnection) connectionLayerModel).registerListener(this, MessageType.AUTH_ACK);
     connectionLayerModel.sendMessage(messageJson);
+    FrontEnd.getView().showLoadingView(false);
   }
 
 
   public UserChatModelLayer(String userToChat) {
     this.userToChatWith = userToChat;
+  }
+
+
+  @Override
+  public void listen(String message) {
+    FrontEnd.getView().showLoadingView(true);
+    
   }
 }
