08d76333e5a8557fe04e2f898e20f73740b087da
mitresh <pandya.mi@husky.neu.edu>
2019-03-15 05:31:59 -0400

    removed code smells

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/NetworkConnection.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/NetworkConnection.java
@@ -43,15 +43,6 @@ public class NetworkConnection implements Iterable<MessageJson> {
 	private static final String MESSAGE_START_KEY = "#{";
 	
 	private static final String MESSAGE_END_KEY = "}#";
-	
-	/** The base for number conversions. */
-	private static final int DECIMAL_RADIX = 10;
-
-	/** The length of the message handle. */ // MEJ: why is this not in Message?
-	private static final int HANDLE_LENGTH = 3;
-
-	/** The minimum length of a message. */ // MEJ: why is this not in Message?
-	private static final int MIN_MESSAGE_LENGTH = 7;
 
 	/** The default character set. */
 	private static final String CHARSET_NAME = "us-ascii";
@@ -184,123 +175,82 @@ public class NetworkConnection implements Iterable<MessageJson> {
 	   * @version 12-27-2018
 	   */
 	  private class MessageIterator implements Iterator<MessageJson> {
-		  
 
-	    /** Default constructor. */
-	    public MessageIterator() {
+		  public MessageIterator() {
+			  // Default constructor.
+		  }
 
-	    }
+		  @Override
+		  public boolean hasNext() {
+			  boolean result = false;
+			  try {
+				  // If we have messages waiting for us, return true.
+				  if (!messages.isEmpty()) {
+					  result = true;
+				  }
+				  // Otherwise, check if we can read in at least one new message
+				  else if (selector.selectNow() != 0) {
+					  assert key.isReadable();
+					  // Read in the next set of commands from the channel.
+					  int dataRead = channel.read(buff);
+					  //Check if client in dead
+					  if (dataRead < 0) {
+						  //Client is dead, add a logout message
+						  messages.add(new MessageJson(MessageType.LOG_OUT));
+					  } else {
+						  selector.selectedKeys().remove(key);
+						  buff.flip();
+						  // Create a decoder which will convert our traffic to something useful
+						  Charset charset = Charset.forName(CHARSET_NAME);
+						  CharsetDecoder decoder = charset.newDecoder();
+						  // Convert the buffer to a format that we can actually use.
+						  CharBuffer charBuffer = decoder.decode(buff);
+						  messageBuffer.append(charBuffer);
+						  // Move all of the remaining data to the start of the buffer.
+						  buff.compact();
+					  }
+					  result = decodeMessage();
+				  }
+			  } catch (IOException ioe) {
+				  // For the moment, we will cover up this exception and hope it never occurs.
+				  assert false;
+			  }
+			  return result;
+		  }
 
-	    @Override
-		public boolean hasNext() {
-			boolean result = false;
-			try {
-				// If we have messages waiting for us, return true.
-				if (!messages.isEmpty()) {
-					result = true;
-				}
-				// Otherwise, check if we can read in at least one new message
-				else if (selector.selectNow() != 0) {
-					assert key.isReadable();
-					// Read in the next set of commands from the channel.
-					int dataRead = channel.read(buff);
-					//Check if client in dead
-					if (dataRead < 0) {
-						//Client is dead, add a logout message
-						messages.add(new MessageJson(MessageType.LOG_OUT));
-					} else {
-						selector.selectedKeys().remove(key);
-						buff.flip();
-						// Create a decoder which will convert our traffic to something useful
-						Charset charset = Charset.forName(CHARSET_NAME);
-						CharsetDecoder decoder = charset.newDecoder();
-						// Convert the buffer to a format that we can actually use.
-						CharBuffer charBuffer = decoder.decode(buff);
-						messageBuffer.append(charBuffer);
-						// Move all of the remaining data to the start of the buffer.
-						buff.compact();
-					}
-					result = decodeMessage();
-				}
-			} catch (IOException ioe) {
-				// For the moment, we will cover up this exception and hope it never occurs.
-				assert false;
-			}
-			return result;
-		}
-	    
-	    @Override
-	    public MessageJson next() {
-	      if (messages.isEmpty()) {
-	        throw new NoSuchElementException("No next line has been typed in at the keyboard");
-	      }
-	      MessageJson msg = messages.remove();
-	      ChatLogger.info(msg.toString());
-	      return msg;
-	    }
-	    
-		private boolean decodeMessage() {
-			boolean isMessageDecoded = false;
-			if (messageBuffer.length() > 0) {
-				String msgBufferString = messageBuffer.toString();
-				if (msgBufferString.contains(MESSAGE_START_KEY) && msgBufferString.contains(MESSAGE_END_KEY)) {
-					int start = msgBufferString.indexOf(MESSAGE_START_KEY) + 1;
-					int end = msgBufferString.indexOf(MESSAGE_END_KEY) + 1;
-					String msg = msgBufferString.substring(start, end);
-					try {
-						// Extract message
-						MessageJson extractedMessage = gson.fromJson(msg, MessageJson.class);
-						isMessageDecoded = true;
-						messages.add(extractedMessage);
-					} catch (JsonSyntaxException e) {
-						ChatLogger.error(e.getMessage());
-					} finally {
-						// remove the particular message extracted
-						messageBuffer.delete(start - 1, end + 1);
-					}
-				}
+		  @Override
+		  public MessageJson next() {
+			  if (messages.isEmpty()) {
+				  throw new NoSuchElementException("No next line has been typed in at the keyboard");
+			  }
+			  MessageJson msg = messages.remove();
+			  ChatLogger.info(msg.toString());
+			  return msg;
+		  }
 
-			}
-			return isMessageDecoded;
-		}
-	    
-	    /**
-	     * Read in a new argument from the IM server.
-	     * 
-	     * @param charBuffer Buffer holding text from over the network.
-	     * @return String holding the next argument sent over the network.
-	     */
-	    private String readArgument(CharBuffer charBuffer) {
-	        String result = null;
-	        // Compute the current position in the buffer
-	        int pos = charBuffer.position();
-	        // Compute the length of this argument
-	        int length = 0;
-	        // Track the number of locations visited.
-	        int seen = 0;
-	        // Assert that this character is a digit representing the length of the first
-	        // argument
-	        assert Character.isDigit(charBuffer.get(pos));
-	        // Now read in the length of the first argument
-	        while (Character.isDigit(charBuffer.get(pos))) {
-	            // My quick-and-dirty numeric converter
-	            length = length * DECIMAL_RADIX;
-	            length += Character.digit(charBuffer.get(pos), DECIMAL_RADIX);
-	            // Move to the next character
-	            pos += 1;
-	            seen += 1;
-	        }
-	        seen += 1;
-	        if (length == 0) {
-	            // Update our position
-	            charBuffer.position(pos);
-	        } else {
-	            // Length is greater than 0 so result should be something other than null
-	            result = charBuffer.subSequence(seen, length + seen).toString();
-	            charBuffer.position(pos + length);
-	        }
-	        return result;
-	    }
-	    
+		  private boolean decodeMessage() {
+			  boolean isMessageDecoded = false;
+			  if (messageBuffer.length() > 0) {
+				  String msgBufferString = messageBuffer.toString();
+				  if (msgBufferString.contains(MESSAGE_START_KEY) && msgBufferString.contains(MESSAGE_END_KEY)) {
+					  int start = msgBufferString.indexOf(MESSAGE_START_KEY) + 1;
+					  int end = msgBufferString.indexOf(MESSAGE_END_KEY) + 1;
+					  String msg = msgBufferString.substring(start, end);
+					  try {
+						  // Extract message
+						  MessageJson extractedMessage = gson.fromJson(msg, MessageJson.class);
+						  isMessageDecoded = true;
+						  messages.add(extractedMessage);
+					  } catch (JsonSyntaxException e) {
+						  ChatLogger.error(e.getMessage());
+					  } finally {
+						  // remove the particular message extracted
+						  messageBuffer.delete(start - 1, end + 1);
+					  }
+				  }
+
+			  }
+			  return isMessageDecoded;
+		  }
 	  }
 }
