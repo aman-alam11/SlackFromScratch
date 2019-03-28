601345ec73295c11088305e0dc5ceea7b772b34e
Kumar Baibhav <baibhav.k@husky.neu.edu>
2019-03-28 19:37:57 -0400

    MSD212SP19-64: group search

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/business/logic/GroupSearchHandler.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/business/logic/GroupSearchHandler.java
@@ -6,6 +6,8 @@ import com.google.gson.Gson;
 
 import edu.northeastern.ccs.im.database.Group;
 import edu.northeastern.ccs.im.database.JPAService;
+import edu.northeastern.ccs.im.message.MessageJson;
+import edu.northeastern.ccs.im.message.MessageType;
 import edu.northeastern.ccs.im.model.GroupSearchModel;
 import edu.northeastern.ccs.im.server.Connection;
 
@@ -21,8 +23,18 @@ public class GroupSearchHandler implements MessageHandler {
 	public boolean handleMessage(String user, String message, Connection clientConnection) {
 		
 		GroupSearchModel groupSearch = gson.fromJson(message, GroupSearchModel.class);
-		//List<Group> groupList = JPAService.getInstance().searchGroupByName(groupSearch.)
-		return false;
+		List<Group> groupList = JPAService.getInstance().allGroupsForUser(user, groupSearch.getToChatWithGroupName());
+		
+		GroupSearchModel searchResult = new GroupSearchModel(groupSearch.getToChatWithGroupName());
+		MessageJson msgJson = new MessageJson(MessageType.GROUP_SERACH);
+		if (groupList != null && !groupList.isEmpty()) {
+			for (Group group : groupList) {
+				searchResult.getListGroupString().add(group.getgName());
+			}
+		}
+		msgJson.setMessage(gson.toJson(searchResult));
+		sendResponse(msgJson, clientConnection);
+		return true;
 	}
 
 }
