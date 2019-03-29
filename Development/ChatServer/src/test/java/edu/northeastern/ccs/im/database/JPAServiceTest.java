f6392976570040f0b6bbbb1b70842fc3a69e3f07
mitresh <pandya.mi@husky.neu.edu>
2019-03-28 21:23:42 -0400

    added database tests and changed the code to test

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/database/JPAServiceTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/database/JPAServiceTest.java
@@ -2,10 +2,8 @@ package edu.northeastern.ccs.im.database;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 
 import java.util.Date;
 
@@ -14,11 +12,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("all")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JPAServiceTest {
-  private SessionFactory sessionFactory;
+  private static SessionFactory sessionFactory;
 
-  @Before
-  public void before() {
+  @BeforeClass
+  public static void before() {
     // setup the session factory
     sessionFactory = new Configuration().
             configure().
@@ -30,23 +29,27 @@ public class JPAServiceTest {
   }
 
   @Test
-  public void testCreateUser() {
+  public void testA() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
       jpaS.createUser("Alice", "a@a.com", "alice");
-//      assertEquals(1, jpaS.readAllUsers().size());
+      jpaS.createUser("Bob","b@b.com","bob");
+      jpaS.createUser("Charlie","c@c.com","charlie");
+      jpaS.createUser("Ali", "b@b.com", "bob");
+      jpaS.createUser("Alike", "c@c.com", "charlie");
+      jpaS.createUser("Allison", "d@c.com", "charliec");
+      assertEquals(6, jpaS.readAllUsers().size());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
   }
 
   @Test
-  public void testDeleteUser() {
+  public void testZ() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
       jpaS.deleteUser(1);
-//    assertEquals(0, jpaS.readAllUsers().size());
+    assertEquals(5, jpaS.readAllUsers().size());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
@@ -63,10 +66,18 @@ public class JPAServiceTest {
   }
 
   @Test
+  public void testCreateUserWithNullEmail() {
+      JPAService jpaS = new JPAService(sessionFactory);
+      jpaS.createUser("a", null, "asdf");
+      assertEquals("",jpaS.findUserByName("a").getEmail());
+      jpaS.deleteUser(jpaS.findUserByName("a").getId());
+  }
+
+  @Test
   public void testCreateUserWithNullPassword() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("alice", "a@a.com", null);
+      jpaS.createUser("alices", "a@a.com", null);
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("Username and Password can't be null"));
     }
@@ -76,24 +87,22 @@ public class JPAServiceTest {
   public void testFindUser() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
       User alice = jpaS.findUserByName("Alice");
-//    assertEquals("a@a.com", alice.getEmail());
+    assertEquals("a@a.com", alice.getEmail());
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("could not extract ResultSet"));
     }
   }
 
   @Test
-  public void testUpdateUser() {
+  public void testX() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
       User alice = jpaS.findUserByName("Alice");
-//      assertEquals("a@a.com", alice.getEmail());
+      assertEquals("a@a.com", alice.getEmail());
       jpaS.updateUser(alice.getId(), alice.getName(), "alice@new.com", alice.getPassword());
       User newAlice = jpaS.findUserByName("Alice");
-//      assertEquals("alice@new.com", newAlice.getEmail());
+      assertEquals("alice@new.com", newAlice.getEmail());
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("could not extract ResultSet"));
     }
@@ -103,19 +112,18 @@ public class JPAServiceTest {
   public void testHashForUser() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-//    assertEquals("", jpaS.getHashFromUsername(null));
+    assertEquals("", jpaS.getHashFromUsername(null));
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
   }
 
   @Test
-  public void testNullUserEmail() {
+  public void testW() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", null, "alice");
       User alice = jpaS.findUserByName("Alice");
-//    assertEquals("", alice.getEmail());
+      assertEquals("a@a.com", alice.getEmail());
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("could not extract ResultSet"));
     }
@@ -125,7 +133,7 @@ public class JPAServiceTest {
   public void testNullEmailGet() {
     try {
       User tempUesr = new User();
-//    assertEquals("", tempUesr.getEmail());
+    assertEquals("", tempUesr.getEmail());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
@@ -136,55 +144,46 @@ public class JPAServiceTest {
     try {
       User tempUesr = new User();
       tempUesr.setEmail(null);
-//    assertEquals("", tempUesr.getEmail());
+    assertEquals("", tempUesr.getEmail());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
   }
 
-//    @Test
-//    public void testCreateChatMessage(){
-//        JPAService jpaS = new JPAService(sessionFactory);
-//        jpaS.createUser("Alice","a@a.com","alice");
-//        jpaS.createUser("Bob","b@b.com","bob");
-//        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
-//        assertEquals("hey there",jpaS.findByReceiver("Bob").get(0).getMsg());
-//    }
+    @Test
+    public void testCreateChatMessage(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
+        assertEquals("hey there",jpaS.findByReceiver("Bob").get(0).getMsg());
+    }
 
-//    @Test
-//    public void testFindAllMessagesOfUser(){
-//        JPAService jpaS = new JPAService(sessionFactory);
-//        jpaS.createUser("Alice","a@a.com","alice");
-//        jpaS.createUser("Bob","b@b.com","bob");
-//        jpaS.createUser("Charlie","c@c.com","charlie");
-//
-//        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
-//        jpaS.createChatMessage("Charlie", "Bob", "How are you?",0, new Date(), false, true);
-//
-//        assertEquals(2,jpaS.findByReceiver("Bob").size());
-//        jpaS.deleteChatByReceiver("Bob");
-//        assertEquals(0,jpaS.findByReceiver("Bob").size());
-//    }
+    @Test
+    public void testFindAllMessagesOfUser(){
+        JPAService jpaS = new JPAService(sessionFactory);
 
-//    @Test
-//    public void testDeleteParticularMessage(){
-//        JPAService jpaS = new JPAService(sessionFactory);
-//        jpaS.createUser("Alice","a@a.com","alice");
-//        jpaS.createUser("Bob","b@b.com","bob");
-//
-//        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
-//        jpaS.createChatMessage("Charlie", "Bob", "How are you?",0, new Date(), false, true);
-//        assertEquals(1,jpaS.findByReceiver("Bob").size());
-//        jpaS.deleteMessage(1);
-//        assertEquals(0,jpaS.findByReceiver("Bob").size());
-//    }
+        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
+        jpaS.createChatMessage("Charlie", "Bob", "How are you?",0, new Date(), false, true);
+
+        assertEquals(4,jpaS.findByReceiver("Bob").size());
+        jpaS.deleteChatByReceiver("Bob");
+        assertEquals(0,jpaS.findByReceiver("Bob").size());
+    }
+
+    @Test
+    public void testDeleteParticularMessage(){
+        JPAService jpaS = new JPAService(sessionFactory);
+
+        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, true);
+        jpaS.createChatMessage("Charlie", "Bob", "How are you?",0, new Date(), false, true);
+        assertEquals(3,jpaS.findByReceiver("Bob").size());
+        jpaS.deleteMessage(1);
+        assertEquals(2,jpaS.findByReceiver("Bob").size());
+    }
 
   @Test
   public void testChatGetters() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
-      jpaS.createUser("Bob", "b@b.com", "bob");
 
       User alice = jpaS.findUserByName("Alice");
       User bob = jpaS.findUserByName("Bob");
@@ -201,22 +200,22 @@ public class JPAServiceTest {
       c.setGrpMsg(false);
       c.setIsDelivered(true);
 
-//    assertEquals(1, c.getId());
-//    assertEquals(1, c.getFromId().getId());
-//    assertEquals(2, c.getToId().getId());
-//    assertEquals(toSave, c.getCreated());
-//    assertEquals(toSave, c.getExpiry());
-//    assertEquals("Hi", c.getMsg());
-//    assertEquals(0, c.getReplyTo());
-//    assertEquals(false, c.getGrpMsg());
-//    assertEquals(true, c.getIsDelivered());
+    assertEquals(1, c.getId());
+    assertEquals(1, c.getFromId().getId());
+    assertEquals(2, c.getToId().getId());
+    assertEquals(toSave, c.getCreated());
+    assertEquals(toSave, c.getExpiry());
+    assertEquals("Hi", c.getMsg());
+    assertEquals(0, c.getReplyTo());
+    assertEquals(false, c.getGrpMsg());
+    assertEquals(true, c.getIsDelivered());
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("could not extract ResultSet"));
     }
   }
 
   @Test
-  public void testDeleteNonExisting() {
+  public void testY() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
       jpaS.deleteUser(1);
@@ -281,11 +280,7 @@ public class JPAServiceTest {
   public void testSearchUser() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
-      jpaS.createUser("Ali", "b@b.com", "bob");
-      jpaS.createUser("Alike", "c@c.com", "charlie");
-      jpaS.createUser("Allison", "d@c.com", "charliec");
-//    assertEquals(3, jpaS.searchUserbyUserName("Ali").size());
+    assertEquals(3, jpaS.searchUserbyUserName("Ali").size());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
@@ -295,49 +290,33 @@ public class JPAServiceTest {
   public void testEmptyUserSearch() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
-//      assertEquals(0, jpaS.searchUserbyUserName("f").size());
+      assertEquals(0, jpaS.searchUserbyUserName("f").size());
     } catch (Exception e) {
       assertEquals("", e.getMessage());
     }
   }
 
-//    @Test
-//    public void testUpdateChatStatus(){
-//        JPAService jpaS = new JPAService(sessionFactory);
-//        jpaS.createUser("Alice","a@a.com","alice");
-//        jpaS.createUser("Bob","b@b.com","bob");
-//        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, false);
-//
-//        jpaS.updateChatStatus(1,true);
-//
-//        assertEquals(true,jpaS.findByReceiver("Bob").get(0).getIsDelivered());
-//    }
+    @Test
+    public void testUpdateChatStatus(){
+        JPAService jpaS = new JPAService(sessionFactory);
+        jpaS.createChatMessage("Alice", "Bob", "hey there",0, new Date(), false, false);
+        jpaS.updateChatStatus(jpaS.findByReceiver("Bob").get(0).getId(),true);
+        assertEquals(true,jpaS.findByReceiver("Bob").get(0).getIsDelivered());
+    }
 
   @Test
   public void testCheck() {
     try {
       JPAService jpaS = new JPAService(sessionFactory);
-      jpaS.createUser("Alice", "a@a.com", "alice");
       User u = jpaS.findUserByName("Alice");
-//    assertEquals("a@a.com", u.getEmail());
+    assertEquals("a@a.com", u.getEmail());
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("could not extract ResultSet"));
     }
   }
 
   @AfterClass
-  public static void close() {
-    try {
-      JPAService jpaS = new JPAService();
-      jpaS.closeSessionFactory();
-    } catch (Exception e) {
-      assertEquals("", e.getMessage());
-    }
-  }
-
-  @After
-  public void after() {
+  public static void after() {
     try {
       sessionFactory.close();
     } catch (Exception e) {
