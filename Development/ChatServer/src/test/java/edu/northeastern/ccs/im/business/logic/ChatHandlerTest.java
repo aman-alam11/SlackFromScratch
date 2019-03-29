9e1bb85322e6203b6c746bbfea13ab414048fc45
Attinderpal Singh <attinder.saini@gmail.com>
2019-03-29 02:53:29 -0400

    added chat handler test

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/business/logic/ChatHandlerTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/business/logic/ChatHandlerTest.java
@@ -6,11 +6,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 
 import edu.northeastern.ccs.im.clientmenu.models.UserChat;
+import edu.northeastern.ccs.im.database.JPAService;
 import edu.northeastern.ccs.im.server.Connection;
 import edu.northeastern.ccs.im.server.business.logic.ChatHandler;
 
@@ -23,23 +27,49 @@ public class ChatHandlerTest {
   @Mock
   private Connection connection;
 
+  @Mock
+  private JPAService jpaService;
+
 
   @Before
   public void init() {
     MockitoAnnotations.initMocks(this);
+    JPAService.setJPAService(jpaService);
     chatHandler = new ChatHandler();
     mGson = new Gson();
   }
 
   @Test
-  public void handleMessageTest() {
-//    UserChat userChat = new UserChat();
-//    userChat.setFromUserName("atti");
-//    userChat.setMsg("hello");
-//    userChat.setToUserName("rsa");
-//    String message = mGson.toJson(userChat);
-//
-//    boolean b = chatHandler.handleMessage("user",message,connection);
-//    assertFalse(b);
+  public void handleMessageTrueTest() {
+    UserChat userChat = new UserChat();
+    userChat.setFromUserName("atti");
+    userChat.setMsg("hello");
+    userChat.setToUserName("rsa");
+    String message = mGson.toJson(userChat);
+
+    long num  = 1;
+    when(JPAService.getInstance().createChatMessage(any())).thenReturn(num);
+
+    boolean b = chatHandler.handleMessage("user",message,connection);
+    assertTrue(b);
+
+
+  }
+
+  @Test
+  public void handleMessageFalseTest() {
+    UserChat userChat = new UserChat();
+    userChat.setFromUserName("atti");
+    userChat.setMsg("hello");
+    userChat.setToUserName("rsa");
+    String message = mGson.toJson(userChat);
+
+    long num  = 0;
+    when(JPAService.getInstance().createChatMessage(any())).thenReturn(num);
+
+    boolean b = chatHandler.handleMessage("user",message,connection);
+    assertFalse(b);
+
+
   }
 }
