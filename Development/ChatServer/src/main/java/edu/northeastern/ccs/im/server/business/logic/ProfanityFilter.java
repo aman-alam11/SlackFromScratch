c553e3232d99e6aefaf77f02d5447b0da56f8ad4
Attinderpal Singh <attinder.saini@gmail.com>
2019-04-09 06:03:03 -0400

    MSD212SP19-79 filter added on group/ user chat

---
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/business/logic/ProfanityFilter.java
@@ -0,0 +1,4 @@
+package edu.northeastern.ccs.im.server.business.logic;
+
+public class ProfanityFilter {
+}
---
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/business/logic/ProfanityFilterTest.java
@@ -0,0 +1,4 @@
+package edu.northeastern.ccs.im.business.logic;
+
+public class ProfanityFilterTest {
+}
