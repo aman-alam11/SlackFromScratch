e0e887e1fb49aab8e7c137e9a87c899335fd025f
Attinderpal Singh <attinder.saini@gmail.com>
2019-04-09 06:03:12 -0400

    MSD212SP19-79 filter added on group/ user chat

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/business/logic/ProfanityFilter.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/business/logic/ProfanityFilter.java
@@ -1,4 +1,59 @@
 package edu.northeastern.ccs.im.server.business.logic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ProfanityFilter {
+  private final static String SINGLESPACE = " ";
+
+  private static ProfanityFilter profanityFilter;
+  private static Set<String> wordSet;
+
+  private ProfanityFilter() {
+    addWords();
+  }
+
+  public static ProfanityFilter getInstace() {
+    if (profanityFilter == null) {
+      wordSet = new HashSet<>();
+      profanityFilter = new ProfanityFilter();
+    }
+    return profanityFilter;
+  }
+
+  private static boolean containsWord(String word) {
+    return wordSet.contains(word.toLowerCase());
+  }
+
+  public static String filterMessage(String word) {
+    String[] wordList = word.split(SINGLESPACE);
+
+    for(int i = 0; i < wordList.length; i++) {
+      if (containsWord(wordList[i])) {
+        wordList[i] = countAndReplace(wordList[i]);
+      }
+    }
+    StringBuilder stringBuilder = new StringBuilder();
+    for (String str: wordList) {
+      stringBuilder.append(str);
+      stringBuilder.append(SINGLESPACE);
+    }
+    return stringBuilder.toString();
+  }
+
+  private void addWords() {
+    wordSet.add("fuck");
+    wordSet.add("ass");
+    wordSet.add("bitch");
+  }
+
+  private static String countAndReplace(String str)  {
+    int length = str.length();
+    StringBuilder stringBuilder = new StringBuilder();
+    for(int i = 0; i < length; i++) {
+      stringBuilder.append("*");
+    }
+    return stringBuilder.toString();
+  }
+
 }
--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/business/logic/ProfanityFilterTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/business/logic/ProfanityFilterTest.java
@@ -1,4 +1,25 @@
 package edu.northeastern.ccs.im.business.logic;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import edu.northeastern.ccs.im.server.business.logic.ProfanityFilter;
+
+
 public class ProfanityFilterTest {
+
+  private ProfanityFilter profanityFilter;
+
+
+
+  @Before
+  public void init() {
+    profanityFilter = ProfanityFilter.getInstace();
+  }
+
+  @Test
+  public void filterTest() {
+    String testStr = "hey ass clown";
+    Assert.assertEquals("hey *** clown ", profanityFilter.filterMessage(testStr));
+  }
 }
